699bbecfb0276cf5d64bbf5b5d127de4eaff29b9
rahulsadanand <rahulsesshoumaro@gmail.com>
2019-01-18 02:20:38 +0530

    homework01
    just basic variable declarations and printing them out with the help of console

---
+++ homework01.js
@@ -0,0 +1,29 @@
+/* favourite song= azadi*/
+
+
+//variable declaration
+
+var name="apna-time-aayega"	 //type:string
+var genre="hip-hop" 		 //type:string
+var singer="ranveer-singh"	 //type:string
+var release_date="14"		 //type:string
+var release_month="02"		 //type:string
+var release_year="2019"		 //type:string
+var album="gully-boy"		 //type:string
+var duration=130		 //type:int
+var duration_unit="secs"	 //type:string
+var lyricist="divine,ankur-tiwari"//type:string
+var music="dubsharma,divine"	 //type:string
+
+
+//printing values within variables
+console.log(name)
+console.log(genre)
+console.log(singer)
+console.log(lyricist)
+console.log(release_date)
+console.log(console_month)
+console.log(release_year)
+console.log(album)
+console.log(duration)
+console.log(duration_unit)
